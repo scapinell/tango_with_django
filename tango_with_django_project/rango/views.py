2b505c260442b9a72d13718889d6f268c95a94ad
iskander <oldautist@gmail.com>
2020-07-10 01:23:58 +0300

    some experiments and added templates

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index0(request):
+    html = "WELCOME" + '<p><a href="/rango/about">About</a></p>' + '<p><a href="/rango">Oh</a></p>'
+    return HttpResponse(html)
 
 
 def index(request):
-    return HttpResponse("Hey there pals")
+    context_dict = {'boldmessage': "Cookie, candy, pancake!"}
+    return render(request, 'rango/index.html', context=context_dict)
+
+
+def about(request):
+    return render(request, 'rango/about.html')
--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -18,9 +18,11 @@ from django.urls import path
 from rango import views
 from django.conf.urls import url
 from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index0, name='index'),
     url(r'^rango/', include('rango.urls'))  # any url starting with rango/ be handled with a rango app
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

cd9b57d55eb8dbd52260148ef3278220e5d4be66
iskander <oldautist@gmail.com>
2020-07-11 23:13:03 +0300

    put the categories names sorted by likes on /rango page (changed relative template and index0 in views

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rango.models import Category
 
 
 def index0(request):
-    html = "WELCOME" + '<p><a href="/rango/about">About</a></p>' + '<p><a href="/rango">Oh</a></p>'
-    return HttpResponse(html)
+    # html = "WELCOME" + '<p><a href="/rango/about">About</a></p>' + '<p><a href="/rango">Oh</a></p>'
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+    return render(request, 'rango/index.html', context=context_dict)
 
 
 def index(request):

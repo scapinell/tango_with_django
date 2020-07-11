3b3fb608ec37f1a3ceaa037ce953851e6b1dba44
iskander <oldautist@gmail.com>
2020-07-12 00:03:17 +0300

    updated views by adding show_category (and made html view for it), added field slug to Category and light changes to admin

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from rango.models import Category
+from rango.models import Page
 
 
 def index0(request):
@@ -17,3 +18,16 @@ def index(request):
 
 def about(request):
     return render(request, 'rango/about.html')
+
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.object.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['pages'] = None
+        context_dict['category'] = None
+    return render(request, 'rango/category.html', context=context_dict)
